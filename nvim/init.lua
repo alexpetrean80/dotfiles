93487dff46c4c6c6530e32ea5ee03f0ecceb14b4
Alex Petrean <alex.petrean@outlook.com>
2023-07-31 12:17:17 +0300

    formatter

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,25 +2,25 @@ local fn = vim.fn
 local inspect = vim.inspect
 
 if fn.has("nvim-0.8") == 0 then
-  error("Need Neovim 0.8+ in order to use this config")
+	error("Need Neovim 0.8+ in order to use this config")
 end
 
 for _, cmd in ipairs({ "git", "rg", { "fd", "fdfind" }, "cmake", "luarocks", "fzf" }) do
-  local name = type(cmd) == "string" and cmd or inspect(cmd)
-  local commands = type(cmd) == "string" and { cmd } or cmd
-  ---@cast commands string[]
-  local found = false
+	local name = type(cmd) == "string" and cmd or inspect(cmd)
+	local commands = type(cmd) == "string" and { cmd } or cmd
+	---@cast commands string[]
+	local found = false
 
-  for _, c in ipairs(commands) do
-    if fn.executable(c) == 1 then
-      name = c
-      found = true
-    end
-  end
+	for _, c in ipairs(commands) do
+		if fn.executable(c) == 1 then
+			name = c
+			found = true
+		end
+	end
 
-  if not found then
-    error(("`%s` is not installed"):format(name))
-  end
+	if not found then
+		error(("`%s` is not installed"):format(name))
+	end
 end
 
 -- Load main config
