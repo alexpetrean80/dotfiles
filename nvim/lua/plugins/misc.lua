369806c0b0737c612f04cc8e6b4fd123521babd5
Alex Petrean <alex@padfoot.cc>
2023-10-17 14:16:16 +0300

    remove neogit, let's just try lazygit

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "f-person/git-blame.nvim",
+    event = "BufRead",
+    config = function()
+      vim.cmd("highlight default link gitblame SpecialComment")
+      vim.g.gitblame_enabled = 0
+    end,
+  },
+  {
     "smjonas/inc-rename.nvim",
     config = function()
       require("inc_rename").setup()
