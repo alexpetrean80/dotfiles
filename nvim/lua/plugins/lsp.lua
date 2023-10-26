e455ad2fc6c00e1faddbb67598360201c36488f1
Alex Petrean <alex@padfoot.cc>
2023-10-26 17:04:17 +0300

    refactor lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,38 +9,46 @@ return {
       "hrsh7th/nvim-cmp",
     },
     config = function()
-      local lsp = require("lsp-zero").preset({})
+      local lsp_zero = require("lsp-zero").preset({})
 
-      lsp.on_attach(function(_, bufnr)
-        lsp.default_keymaps({ buffer = bufnr })
+      lsp_zero.on_attach(function(_, bufnr)
+        lsp_zero.default_keymaps({ buffer = bufnr })
       end)
 
-      require("lspconfig").lua_ls.setup({
-        Lua = {
-          runtime = {
-            version = "LuaJIT",
-          },
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),
-          },
-          telemetry = {
-            enable = false,
-          },
-        },
-      })
+      local lsp = require("lspconfig")
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      lsp.lua_ls.setup(lua_opts)
 
-      lsp.setup()
+      -- local configs = require("lspconfig.configs")
+      --
+      -- if not configs.snyk then
+      --   configs.snyk = {
+      --     default_config = {
+      --       -- pass cmd as .local/bin/snyk-lsp in my $HOME
+      --       cmd = { "snyk-ls" },
+      --       root_dir = function(name)
+      --         return lsp.util.find_git_ancestor(name) or vim.loop.os_homedir()
+      --       end,
+      --       init_options = {
+      --         activateSnykCode = "true",
+      --       },
+      --     },
+      --   }
+      -- end
+      --
+      -- lsp.snyk.setup({
+      --   on_attach = lsp_zero.on_attach,
+      -- })
+
+      lsp_zero.setup()
     end,
   },
-	{
-		"smjonas/inc-rename.nvim",
-		config = function()
-			require("inc_rename").setup()
-		end,
-	},
+  {
+    "smjonas/inc-rename.nvim",
+    config = function()
+      require("inc_rename").setup()
+    end,
+  },
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
