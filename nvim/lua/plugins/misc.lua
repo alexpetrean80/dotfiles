12313ad0b03cc12d324b05f562d9a39b78de1a14
Alex Petrean <alex@padfoot.cc>
2023-11-07 16:57:34 +0200

    add fugitive

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,29 +1,30 @@
 return {
-	{
-		"f-person/git-blame.nvim",
-		event = "BufRead",
-		config = function()
-			vim.cmd("highlight default link gitblame SpecialComment")
-			vim.g.gitblame_enabled = 0
-		end,
-	},
-	{ "machakann/vim-sandwich" },
-	{
-		"echasnovski/mini.comment",
-		version = "*",
-		config = function()
-			require("mini.comment").setup({
-				pre_hook = function()
-					return require("ts_context_commentstring.internal").calculate_commentstring()
-				end,
-			})
-		end,
-	},
-	{
-		"echasnovski/mini.pairs",
-		version = false,
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	},
+  { "tpope/vim-fugitive" },
+  {
+    "f-person/git-blame.nvim",
+    event = "BufRead",
+    config = function()
+      vim.cmd("highlight default link gitblame SpecialComment")
+      vim.g.gitblame_enabled = 0
+    end,
+  },
+  { "machakann/vim-sandwich" },
+  {
+    "echasnovski/mini.comment",
+    version = "*",
+    config = function()
+      require("mini.comment").setup({
+        pre_hook = function()
+          return require("ts_context_commentstring.internal").calculate_commentstring()
+        end,
+      })
+    end,
+  },
+  {
+    "echasnovski/mini.pairs",
+    version = false,
+    config = function()
+      require("mini.pairs").setup()
+    end,
+  },
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,34 +1,47 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "marko-cerovac/material.nvim",
     config = function()
-      require("catppuccin").setup({
-        -- flavour = "macchiato",
-        background = {
-          light = "latte",
-          dark = "macchiato",
+      require("material").setup({
+        contrast = {
+          terminal = false,      -- Enable contrast for the built-in terminal
+          sidebars = false,       -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+          floating_windows = true, -- Enable contrast for floating windows
+          cursor_line = true,    -- Enable darker background for the cursor line
+          non_current_windows = true, -- Enable contrasted background for non-current windows
+          filetypes = {},        -- Specify which filetypes get the contrasted (darker) background
         },
-        integrations = {
-          alpha = true,
-          gitsigns = true,
-          mason = true,
-          mini = {
-            enabled = true,
-            indentscope_color = "",
-          },
-          neotree = true,
-          noice = true,
-          cmp = true,
-          lsp_trouble = true,
-          treesitter_context = true,
-          treesitter = true,
-          ts_rainbow2 = true,
-          telescope = true,
-          illuminate = true,
-          sandwich = true,
-          which_key = true,
+
+        plugins = { -- Uncomment the plugins that you use to highlight them
+          "mini",
+          "noice",
+          "nvim-cmp",
+          "nvim-web-devicons",
+          "telescope",
+          "trouble",
+          "which-key",
         },
+
+        disable = {
+          colored_cursor = true, -- Disable the colored cursor
+          borders = true,   -- Disable borders between verticaly split windows
+          background =false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+          term_colors = false, -- Prevent the theme from setting terminal colors
+          eob_lines = false, -- Hide the end-of-buffer lines
+        },
+
+        high_visibility = {
+          lighter = false, -- Enable higher contrast text for lighter style
+          darker = true, -- Enable higher contrast text for darker style
+        },
+
+        lualine_style = "stealth", -- Lualine style ( can be 'stealth' or 'default' )
+
+        async_loading = true,  -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+        custom_colors = nil,   -- If you want to override the default colors, set this to a function
+
+        custom_highlights = {}, -- Overwrite highlights with your own
       })
     end,
   },
@@ -37,13 +50,13 @@ return {
     config = function()
       require("lualine").setup({
         options = {
-          theme = "catppuccin",
+          theme = "material",
           component_separators = "|",
           section_separators = { left = "", right = "" },
         },
         sections = {
           lualine_a = {
-            { "mode", separator = { left = "" }, right_padding = 2 },
+            { "mode", separator = { left = "", right = ""  }, right_padding = 2 },
           },
           lualine_b = { "filename", "branch" },
           lualine_c = {},
