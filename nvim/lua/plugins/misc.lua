c7d6f1c5be071a700883845c1fef294b2975d1db
Alex Petrean <alex@padfoot.cc>
2023-11-20 22:31:17 +0200

    change some kitty stuff

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
     "f-person/git-blame.nvim",
     event = "BufRead",
     config = function()
