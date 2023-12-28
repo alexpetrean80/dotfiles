f955fc352548b16ffa2cff0eda687c222cf38554
Alex Petrean <alex@padfoot.cc>
2023-12-28 18:35:58 +0200

    fix telescope keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,11 +3,12 @@ return {
   tag = "0.1.4",
   dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
   keys = {
-    { "<leader> ", "<cmd>Telescope find_files<CR>", desc = "Find file" },
-    { "<leader>/", "<cmd>Telescope live_grep<CR>",  desc = "Grep" },
+    { "<leader> ",  "<cmd>Telescope find_files<CR>", desc = "Find file" },
+    { "<leader>/",  "<cmd>Telescope live_grep<CR>",  desc = "Grep" },
+    { "<leader>bb", "<cmd>Telescope buffers<CR>",    desc = "Buffers" },
     {
       "<leader>F",
-      "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find({ previewer = false })",
+      "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find({ previewer = false })<CR>",
       desc = "Fuzzy find",
     },
   },
