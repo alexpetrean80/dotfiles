1c077f6d21ca0d26af1e3b752527121925b520f4
Alex Petrean <alex@padfoot.cc>
2023-12-28 18:32:59 +0200

    move stuff around

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,140 +1,79 @@
 return {
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    opts = {
-      show_dirname = false,
-      show_basename = true,
-    },
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
-          light = "latte",
-          dark = "macchiato",
-        },
-        transparent_background = false, -- disables setting the background color.
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false,        -- sets terminal colors (e.g. `g:terminal_color_0`)
-        dim_inactive = {
-          enabled = false,          -- dims the background color of inactive window
-          shade = "dark",
-          percentage = 0.15,        -- percentage of the shade to apply to the inactive window
-        },
-        no_italic = false,          -- Force no italic
-        no_bold = false,            -- Force no bold
-        no_underline = false,       -- Force no underline
-        styles = {                  -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" },  -- Change the style of comments
-          conditionals = { "italic" },
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-        },
-        color_overrides = {},
-        custom_highlights = {},
-        integrations = {
-          cmp = true,
-          barbecue = {
-            dim_dirname = true, -- directory name is dimmed by default
-            bold_basename = true,
-            dim_context = false,
-            alt_background = false,
-          },
-          nvimtree = false,
-          treesitter = true,
-          notify = false,
-          mini = {
-            enabled = true,
-            indentscope_color = "",
-          },
-          noice = true,
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-              errors = { "italic" },
-              hints = { "italic" },
-              warnings = { "italic" },
-              information = { "italic" },
-            },
-            underlines = {
-              errors = { "underline" },
-              hints = { "underline" },
-              warnings = { "underline" },
-              information = { "underline" },
-            },
-            inlay_hints = {
-              background = true,
-            },
-          },
-          ts_rainbow2 = true,
-          telescope = true,
-          lsp_trouble = true,
-          gitsigns = true,
-          sandwich = true,
-          which_key = true,
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-        },
-      })
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			local wc = require("which-key")
 
-      -- setup must be called before loading
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      require("noice").setup({
-        lsp = {
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-        },
-        presets = {
-          command_palette = true,
-          inc_rename = true,
-          lsp_doc_border = true,
-          bottom_search = false,
-          long_message_to_split = true,
-          indent_blankline = {
-            enabled = true,
-            scope_color = "lavender",
-            colored_indent_levels = false,
-          },
-        },
-      })
-    end,
-  },
-  {
-    "echasnovski/mini.files",
-    version = false,
-    config = function()
-      require("mini.files").setup()
-    end,
-  },
+			wc.register({
+				b = { name = "Buffers" },
+				l = { name = "LSP" },
+			}, {
+				mode = "n",
+				prefix = "<leader>",
+				buffer = nil,
+				silent = true,
+				noremap = true,
+				nowait = false,
+			})
+
+			wc.setup({
+				window = { margin = { 1, 0, 0, 0.8 }, border = "single" },
+				layout = { height = { min = 4, max = 150 }, align = "left" },
+			})
+		end,
+	},
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		opts = {
+			show_dirname = false,
+			show_basename = true,
+		},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			require("noice").setup({
+				lsp = {
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				presets = {
+					command_palette = true,
+					inc_rename = true,
+					lsp_doc_border = true,
+					bottom_search = false,
+					long_message_to_split = true,
+					indent_blankline = {
+						enabled = true,
+						scope_color = "lavender",
+						colored_indent_levels = false,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.files",
+		version = false,
+		config = function()
+			require("mini.files").setup()
+		end,
+	},
 }
