9bde6d87044ff4cb56a88a8103934b1a88438674
Alex Petrean <alex@padfoot.cc>
2024-01-03 16:50:32 +0200

    cleanup nvim config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -50,4 +50,115 @@ return {
     },
     opts = {},
   },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "williamboman/mason.nvim", "nvim-lua/plenary.nvim" },
+    lazy = false,
+    config = function()
+      local null_ls = require("null-ls")
+
+      local formatting = null_ls.builtins.formatting
+
+      local diagnostics = null_ls.builtins.diagnostics
+      local code_actions = null_ls.builtins.code_actions
+
+      null_ls.setup({
+        debug = false,
+        sources = {
+          formatting.gofmt,
+          formatting.goimports,
+          formatting.stylua,
+          formatting.black.with({
+            prefer_local = true,
+            cwd = function(params)
+              -- return vim.fn.fnamemodify(params.bufname, ":h")
+              return params.root:match("sw-digilent-cloud")
+            end,
+          }),
+          formatting.isort,
+          formatting.prettier.with({
+            prefer_local = "node_modules/.bin/prettier",
+          }),
+          diagnostics.luacheck,
+          diagnostics.dotenv_linter,
+          diagnostics.editorconfig_checker,
+          diagnostics.flake8.with({
+            prefer_local = true,
+            cwd = function(params)
+              return vim.fn.fnamemodify(params.bufname, ":h")
+            end,
+          }),
+          diagnostics.eslint.with({
+            prefer_local = "node_modules/.bin/eslint",
+          }),
+          diagnostics.gitlint,
+          diagnostics.gdlint,
+          diagnostics.golangci_lint,
+          -- diagnostics.gospel,
+          code_actions.eslint.with({
+            prefer_local = "node_modules/.bin/prettier",
+          }),
+          code_actions.gomodifytags,
+          code_actions.impl,
+          code_actions.refactoring,
+          code_actions.shellcheck,
+        },
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git", "pyproject.toml"),
+      })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason-tool-installer").setup({
+        auto_update = true,
+        run_on_start = true,
+        ensure_installed = {
+          "bashls",
+          "black",
+          "clang-format",
+          "clangd",
+          "cmake",
+          "cpplint",
+          "cssls",
+          "docker_compose_language_service",
+          "dockerls",
+          "editorconfig-checker",
+          "eslint",
+          "flake8",
+          "gitlint",
+          "goimports",
+          "golangci-lint",
+          "golines",
+          "gomodifytags",
+          "gopls",
+          "hadolint",
+          "helm_ls",
+          "html",
+          "isort",
+          "jq",
+          "jsonls",
+          "lua_ls",
+          "luacheck",
+          "marksman",
+          "ocamllsp",
+          "prettier",
+          "pyright",
+          "rust-analyzer",
+          "shellcheck",
+          "shfmt",
+          "sqlls",
+          "stylelint",
+          "stylua",
+          "taplo",
+          "tsserver",
+          "yamllint",
+        },
+      })
+    end,
+  },
 }
