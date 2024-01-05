b5d1a0509d1039f0002b69ce89691833309ce8f6
Alex Petrean <alex@padfoot.cc>
2024-01-05 14:32:03 +0200

    bye copilot

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -83,33 +83,4 @@ return {
       })
     end,
   },
-  {
-    "zbirenbaum/copilot.lua",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        config = function()
-          require("copilot_cmp").setup()
-        end,
-      },
-    },
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      vim.defer_fn(function()
-        require("copilot").setup({
-          panel = {
-            enabled = false,
-          },
-          suggestion = {
-            enabled = false,
-          },
-          filetypes = {
-            ["."] = true,
-            yaml = true,
-          },
-        })
-      end, 100)
-    end,
-  },
 }
