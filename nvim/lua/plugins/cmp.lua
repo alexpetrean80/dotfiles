c672abacabed15a888f648ed115ef7843dcbf5c1
Alex Petrean <alex@padfoot.cc>
2024-01-05 14:50:30 +0200

    bye copilot 2

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -22,7 +22,6 @@ return {
         sorting = {
           priority_weight = 2,
           comparators = {
-            require("copilot_cmp.comparators").prioritize,
             cmp.config.compare.offset,
             cmp.config.compare.exact,
             cmp.config.compare.score,
