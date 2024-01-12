44a405974dd26e372cafca57748b26430751db41
Alex Petrean <alex_petrean@icloud.com>
2024-01-13 00:18:31 +0200

    add auto-dark-mode

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -300,4 +300,20 @@ return {
       t.load_extension("fzf")
     end,
   },
+  {'f-person/auto-dark-mode.nvim', config = function()
+    local auto_dark_mode = require('auto-dark-mode')
+
+    auto_dark_mode.setup({
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option('background', 'dark')
+        vim.cmd('colorscheme catppuccin')
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option('background', 'light')
+        vim.cmd('colorscheme catppuccin')
+      end,
+    })
+  end
+  }
 }
